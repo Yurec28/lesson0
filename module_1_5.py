40b84339fa9001295181716600d87f46f34a6b10
Yurec28 <karapetyan-y@list.ru>
2024-12-19 23:45:01 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var_tuple='gun', 2,'shot', 1, True
+print(immutable_var_tuple)
+#immutable_var_tuple['shot'] = 'peach' #кортеж не поддерживает обращение по элементам
+immutable_var = ['gun', 2,'shot', 1, True]
+immutable_var [0] = 'people'
+immutable_var [-1] = False
+print(immutable_var)
